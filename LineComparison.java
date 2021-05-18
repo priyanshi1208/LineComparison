987771113ee497e5a9af10da32f29495d47a5715
priyanshi1208 <84199742+priyanshi1208@users.noreply.github.com>
2021-05-18 10:53:19 +0530

    Update LineComparison.java
    Removed unnecessary code

--- LineComparison.java
+++ LineComparison.java
@@ -7,18 +7,13 @@ import org.apache.logging.log4j.Logger;
 
 
 public class LineComparison {
-	private static final Logger LOG = LogManager.getLogger(App.class);
+	private static final Logger LOG = LogManager.getLogger(LineComparison.class);
 	double Length;
 	public void length(int x1,int y1,int x2, int y2)
 	{
 		  Length = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 		  LOG.debug(Length + " Will be printed on Debug");
-	        LOG.info(Length + " Will be printed on Info");
-	        LOG.warn(Length + " Will be printed on Warn");
-	        LOG.error(Length + " Will be printed on Error");
-	        LOG.fatal(Length + " Will be printed on Fatal");
-	        LOG.info("Appending string: {}.",Length);
-	        System.out.println(Length);
+       	          System.out.println(Length);
 		 
 	}
 	
